c01afbb3f7b2df73eedd52a961ebf0f537fed29b
EC2 Default User <ec2-user@ip-172-31-39-162.ec2.internal>
2019-11-16 23:20:08 +0000

    I was unable to get the sql database up and running, but here's the code for the website that would run if the connection worked

--- main.py
+++ main.py
@@ -8,14 +8,26 @@ class MainPage(webapp2.RequestHandler):
   self.response.headers["Content-Type"] = "text/html"
   self.response.write("Hello World")
 
+#class SQLtest(webapp2.RequestHandler):
+# def get(self):
+#  self.response.headers["Content-Type"] = "text/html"
+#  conn = MySQLdb.connect(unix_socket = "/home/ec2-user/cloudsql/central-apex-259103:us-central1:jtroyergae", user = gauthen.SQL_USER, passwd = gauthen.SQL_PASSWD, db = "labgmain")
+#  cursor = conn.cursor()
+#  cursor.execute("SELCT * FROM test;")
+#  results = cursor.fetchall()
+#  for rec in results:
+#   self.response.write(rec)
+
 class frontpage(webapp2.RequestHandler):
  def get(self):
   self.response.headers["Content-Type"] = "text/html"
   user = ""
   sessionid = 0
+  #First checkf or session.
   if self.request.cookies.get("sessioncookie") == "None":
+   #If there is none, create a cookie
    sessionid = "%032x" % random.getrandbits(128)
-   conn = MySQLdb.connect(host = gauthen.SQL_HOST, user = gauthen.SQL_USER, passwd = gauthen.SQL_PASSWD, db = "labgmain")
+   conn = MySQLdb.connect(unix_socket = gauthen.SQL_SOCKET, user = gauthen.SQL_USER, passwd = gauthen.SQL_PASSWD, db = "labgmain")
    cursor = conn.cursor()
    cursor.execute("INSERT INTO sessions(sessid) VALUES \"" + sessionid + "\";")
    cursor.close()
@@ -23,6 +35,7 @@ class frontpage(webapp2.RequestHandler):
    conn.closer()
    self.response.set_cookie("sessioncookie", sessionid, max_age=1800)
   else:
+   #If there is get it
    sessionid = self.request.cookies.get("sessioncookie")
    conn = MySQLdb.connect(host = gauthen.SQL_HOST, user = gauthen.SQL_USER, passwd = gauthen.SQL_PASSWD, db = "labgmain")
    cursor = conn.cursor()
@@ -31,7 +44,7 @@ class frontpage(webapp2.RequestHandler):
    user = results[0][0]
    cursor.close()
    conn.close()
-
+  #Now get the user. If there is none ask them to create one
   if user == "NULL" or user == "":
    self.response.write("""
 <html>
@@ -41,15 +54,16 @@ class frontpage(webapp2.RequestHandler):
 <body>
 <form action="/landing" method="get">
  <input type=text name=user value="username here">
- <input type=hidden name=id value="""
-self.response.write(sessionid)
-self.response.write(""">
+ <input type=hidden name=id value=""")
+   self.response.write(sessionid)
+   self.response.write(""">
  <input type=submit>
 </form>
 </body>
 </html>
 """)
   else:
+   #If we have a user then show them the plain old button presses page
    self.response.write("""
 <html>
 <head>
@@ -59,22 +73,49 @@ self.response.write(""">
 You've pressed the button
 """)
 
-   conn = MySQLdb.connect(host = gauthen.SQL_HOST, user = gauthen.SQL_USER, passwd = gauthen.SQL_PASSWD, db = "labgmain")
+   conn = MySQLdb.connect(unix_socket = gauthen.SQL_SOCKET, user = gauthen.SQL_USER, passwd = gauthen.SQL_PASSWD, db = "labgmain")
    cursor = conn.cursor()
-   cursor.excecute("SELECT number FROM presses WHERE user='" + user + "';")
+   cursor.execute("SELECT number FROM presses WHERE user=\"" + user + "\";")
    results = cursor.fetchall()
    self.response.write(results[0][0])
    self.response.write("""
 times.<b>
 <form action="/pressed" method="get">
-<input type=submit>""")
+<input type=hidden name=user value=\"""")
+   self.response.write(user)
+   self.response.write("""
+<input type=submit>
+</form>
+</body>
+</html>""")
 
+
+#For creating a new user
 class landingpad(webapp2.RequestHanlder):
  def get(self):
   form = cgi.FieldStorage()
-  conn = MySQLdb.connect(host = gauthen.SQL_HOST, user = gauthen.SQL_USER, passwd = gauthen.SQL_PASSWD, db = "labgmain")
+  conn = MySQLdb.connect(unix_socket = gauthen.SQL_SOCKET, user = gauthen.SQL_USER, passwd = gauthen.SQL_PASSWD, db = "labgmain")
   cursor = conn.cursor()
-  cursor.excecute("INSERT ")
+  form = cgi.FieldStorage()
+  cursor.execute("UPDATE sessions SET username=\"" + form["user"] + "\" WHERE sessid=" + form["id"] + ";")
+  cursor.close()
+  conn.commit()
+  cursor = conn.cursor()
+  cursor.execute("INSERT INTO presses(user, number) VALUES (\"" + form["user"] +"\", 0);")
+  cursor.close()
+  conn.commit()
+  #now redirect back to the main page
+  self.redirect("https://central-apex-259103.appspot.com/")
 
-app = webapp2.WSGIApplication([("/", frontpage), ("/old", MainPage)], debug=True)
+class pressHandler(webapp2.RequestHandler):
+ def get(self):
+  conn = MySQLdb.connect(unix_socket = gauthen.SQL_SOCKET, user = gauthen.SQL_USER, passwd = gauthen.SQL_PASSWD, db = "labgmain")
+  cursor = conn.cursor()
+  form = cgi.FieldStorage()
+  cursor.execute("UPDATE presses SET number = number+1 WHERE user = \"" + form["user"] + "\";")
+  cursor.close()
+  conn.commit()
+  self.redirect("https://central-apex-259103.appspot.com/")
+
+app = webapp2.WSGIApplication([("/", frontpage), ("/landing", landingpad), ("/pressed", pressHandler)], debug=True)
 
